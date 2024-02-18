51faf45db91f5c2ef94b80e1a245952bd1f3e26b
Raden Cedrick  Dosel <raden.dosel00@gmail.com>
2024-02-19 01:06:17 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,13 +10,13 @@ const connectDB = require("./config/db");
 const { errorHandler } = require("./middleware/errorHandler");
 
 //connect to database
+connectDB();
 
 //express app
 const app = express();
 const port = process.env.PORT || 4000; //Port number
 
 //middleware
-app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
