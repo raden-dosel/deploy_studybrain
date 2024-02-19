dd41431ab275aa899bdd2df1ade4d814b6b81a88
Raden Cedrick Dosel <raden.dosel00@gmail.com>
2024-02-19 08:41:50 +0800

    update server

--- backend/server.js
+++ backend/server.js
@@ -35,10 +35,7 @@ app.use(errorHandler);
 app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://deploy-studybrain-frontend-7vcmvt0x8-raden-dosels-projects.vercel.app"
-  );
+  res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
